d251867d42027faa0d72232d8e44877094e6c871
chiiiii123 <161492942+chiiiii123@users.noreply.github.com>
2025-11-08 17:18:57 +0000

    Update GameOverScreen.java

--- Game/core/src/main/java/io/github/team10/escapefromuni/GameOverScreen.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/GameOverScreen.java
@@ -4,15 +4,22 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Texture;
 
 public class GameOverScreen implements Screen {
 
     private final EscapeGame game;
     private BitmapFont font;
+    private boolean isWon;
+    private Texture winScreen;
+    private Texture loseScreen;
 
-    public GameOverScreen(final EscapeGame game) {
+    public GameOverScreen(final EscapeGame game, boolean isWon) {
         this.game = game;
+        this.isWon = isWon;
         font = new BitmapFont();
+        winScreen = new Texture("WinScreen.png");
+        loseScreen = new Texture("LoseScreen.png");
     }
 
     @Override
@@ -22,6 +29,19 @@ public class GameOverScreen implements Screen {
         font.draw(game.batch, "GAME OVER", 200, 250);
         font.draw(game.batch, "Press ESC or close window", 200, 200);
         game.batch.end();
+        if(isWon){
+            renderWinScreen();
+        }
+        else{
+            renderLoseScreen();
+        }
+    }
+
+    private void renderWinScreen(){
+        game.batch.draw(winScreen, 0, 0, game.viewport.getWorldWidth(), game.viewport.getWorldHeight());
+    }
+    private void renderLoseScreen(){
+        game.batch.draw(loseScreen, 0, 0, game.viewport.getWorldWidth(), game.viewport.getWorldHeight());
     }
 
     @Override public void show() {}
