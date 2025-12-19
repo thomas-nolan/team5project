df38f6877401e235085d1c6f7a5c05a8b39c6bc3
mimi <66320738+Mimi1543@users.noreply.github.com>
2025-12-19 15:14:20 +0000

    Create EventsTest.java
    TO DO WHEN CODE UPDATED!!!

---
+++ Game/core/src/main/java/io/github/team10/escapefromuni/testing/EventsTest.java
@@ -0,0 +1,26 @@
+package io.github.team10.escapefromuni.Testing;
+
+
+//Okay so this is not done, as trying to figure out which events are acutally done.
+// There should be around 8 Events in total but i can only find five and i need the counter to
+//test whether its all been run
+//This is a big test case!! testing the events here unit test.
+public class EventsTest {
+    public EventsTest(){}
+
+    //Make boolean when added
+    public void runAllTests() {
+        EventsTest testing = new EventsTest();
+
+        System.out.println("\nEvent System Test!");
+        System.out.println("--------------------");
+
+        //boolean test1 = testing.XTEST();
+
+        System.out.println("\n\nTESTING FINISHED");
+        System.out.println("--------------------");
+
+    }
+
+
+}
