852586184b4ca8a26a86dea592c01f17db95f4f6
afkWill <punticus@gmail.com>
2026-01-03 12:37:26 +0000

    add: tests

---
+++ Game/core/src/test/java/io/github/team10/escapefromuni/ScoreManagerTest.java
@@ -0,0 +1,63 @@
+package io.github.team10.escapefromuni;
+
+import java.beans.Transient;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreManagerTest {
+
+    private ScoreManager scoreManager;
+
+    @BeforeEach
+    void setUp() {
+        scoreManager = new ScoreManager();
+    }
+
+    @Test
+    void calculateFinalScore_calculatesCorrectScore() throws Exception {
+        AchievementManager achievementManager = new AchievementManager();
+
+        scoreManager.increaseScore(10);
+        achievementManager.addAchievement("Complete game");
+        achievementManager.addAchievement("Speedrun");
+
+        int achievementScore = achievementManager.getTotalAchievements();
+        int correctScore = (50 * 30) + (200 * achievementScore) + scoreManager.getScore();
+
+        assertEquals(correctScore, scoreManager.CalculateFinalScore(30, achievementManager));
+    }
+    
+    @Test
+    void increaseScore_addsToScore() {
+        scoreManager.increaseScore(10);
+        assertEquals(10, scoreManager.getScore());
+
+        scoreManager.increaseScore(20);
+        assertEquals(30, scoreManager.getScore());
+    }
+
+    @Test
+    void reset_setsScoreToZero() {
+        scoreManager.increaseScore(10);
+        scoreManager.reset();
+
+        assertEquals(0, scoreManager.getScore());
+    }
+
+    @Test
+    void isDuplicate_returnsTrueIfDuplicate() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Tom", 69);
+
+        assertTrue(scoreManager.isDuplicate(map, "Tom"));
+        assertFalse(scoreManager.isDuplicate(map, "Will"));
+    }
+
+
+}
--- Game/core/src/test/java/io/github/team10/escapefromuni/TimerTest.java
+++ Game/core/src/test/java/io/github/team10/escapefromuni/TimerTest.java
@@ -6,14 +6,20 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TimerTest {
+
+    private Timer timer;
+
+    @BeforeEach
+    void setUp() {
+        timer = new Timer();
+    }
     
     @Test
-    void contrutor_SetsCorrectDefault() { 
-        Timer timer = new Timer();
-
+    void contrutor_setsCorrectDefault() { 
         assertEquals(0f, timer.getTime());
         assertEquals(300f, timer.getTimeLeftSeconds());
         assertFalse(timer.isFinished());
@@ -21,16 +27,68 @@ public class TimerTest {
 
     @Test 
     void update_drecreasesTime() {
-        Timer timer = new Timer();
-
         timer.update(100f);
+
         assertEquals(200f, timer.getTimeLeftSeconds());
     }
 
     @Test 
     void update_increasesElapsedTime() {
-        Timer timer = new Timer();
         timer.update(100f);
+
         assertEquals(100f, timer.getTime());
+    }
+
+    @Test
+    void isFinished_neverIsNegative() {
+        timer.update(1000f);
+
+        assertEquals(0f, timer.getTimeLeftSeconds());
+        assertTrue(timer.isFinished());
+    }
+
+    @Test
+    void hasReached_returnsTrueWhenReached() {
+        timer.update(100f);
+
+        assertTrue(timer.hasReached(50f));
+        assertFalse(timer.hasReached(200f));
+    }
+
+    @Test
+    void isFinished_returnsTrueWhenFinished() {
+        timer.update(300f);
+
+        assertTrue(timer.isFinished());
+    }
+
+    @Test
+    void setFrozen_freezesTimeAndTimeLeft() {
+        timer.setFrozen();
+        timer.update(100f);
+
+        assertEquals(0f, timer.getTime());
+        assertEquals(300, timer.getTimeLeftSeconds());
+    } 
+
+    @Test
+    void frozenTimer_finishesAndTimerContinues() {
+        timer.setFrozen();
+        timer.update(30f);
+        timer.update(10f);
+
+        assertEquals(10f, timer.getTime());
+        assertEquals(290f, timer.getTimeLeftSeconds());
+    }
+
+    @Test 
+    void reset_setsDefaultTimer() {
+        timer.update(100f);
+        timer.setFrozen();
+        timer.reset();
+
+        assertEquals(0f, timer.getTime());
+        assertEquals(300f, timer.getTimeLeftSeconds());
+        assertFalse(timer.isFinished());
     }
 }
