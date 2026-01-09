7daa8046e3f7586083ba9fd234b37c70a9fa6f7f
thomas-nolan <t.nolan2592@gmail.com>
2026-01-09 22:04:22 +0000

    Adding java doc and changing style to google style

--- Game/core/src/main/java/io/github/team10/escapefromuni/Door.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/Door.java
@@ -5,71 +5,95 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 /**
  * Represents a door used to connect rooms.
- * 
  * Each {@code Door} is has a direction and is managed by the {@link DoorController}.
  * The door can be active or inactive indicating whether it is visible and can be used.  
  */
 public class Door {
-    public DoorController doorController;
-    public DoorDirection direction;
+  public DoorController doorController;
+  public DoorDirection direction;
 
-    public boolean isActive;
-    public Texture doorTexture;
-    public Sprite doorSprite;
+  public boolean isActive;
+  public Texture doorTexture;
+  public Sprite doorSprite;
 
-    private boolean isLocked = false;
+  // NEW FOR ASSESSMENT 2 - used for locking doors
+  private boolean isLocked = false;
 
-    /**
-     * Creates a new Door instance.
-     * 
-     * The door is always active initially (but this may be changed by the roomManager).
-     * @param roomManager Manages the door.
-     * @param direction Direction of the door in relation to the center of the room.
-     * @param x The x-coord of the bottom left corner of the door.
-     * @param y The y-coord of the bottom left corner of the door.
-     */
-    public Door(DoorController doorController, DoorDirection direction, float x, float y)
-    {
-        this.doorController = doorController;
-        this.direction = direction;
+  /**
+   * Creates a new Door instance.
+   * The door is always active initially and can be locked.
+   * 
+   * @param direction Direction of the door in relation to the center of the room.
+   * @param x The x-coord of the bottom left corner of the door.
+   * @param y The y-coord of the bottom left corner of the door.
+   */
+  public Door(DoorController doorController, DoorDirection direction, float x, float y) {
+    // initialises the door controller and direction
+    this.doorController = doorController;
+    this.direction = direction;
 
-        doorTexture = new Texture("DoorNew.png");
-        doorSprite = new Sprite(doorTexture);
+    // loads in the texture and sprite
+    doorTexture = new Texture("DoorNew.png");
+    doorSprite = new Sprite(doorTexture);
 
-        doorSprite.setSize(1f, 1f);
-        doorSprite.setPosition(x, y);
+    // sets the size and position of the sprite
+    doorSprite.setSize(1f, 1f);
+    doorSprite.setPosition(x, y);
 
-        isActive = true;
+    isActive = true;
+  }
+ 
+  /**
+   * Draws the door sprite into the game.
+   */
+  public void draw() {
+    if (isActive) {
+      doorSprite.draw(doorController.getGame().batch);    
     }
+  }
 
-    public void draw()
-    {
-        if (isActive)
-        {
-            doorSprite.draw(doorController.getGame().batch);    
-        }
-    }
+  /**
+   * This method sets the door to active or not active.
+   * 
+   * @param isActive boolean value containing the active preference
+   */
+  public void setActive(boolean isActive) {
+    this.isActive = isActive;
+  }
 
-    public void setActive(boolean isActive)
-    {
-        this.isActive = isActive;
-    }
+  /**
+   * This method gets the status of the door.
+   * 
+   * @return true if the door is active, false otherwise
+   */
+  public boolean getActive() {
+    return isActive;
+  }
 
-    public boolean getActive()
-    {
-        return isActive;
-    }
-
-    public void setLocked(boolean locked){
-        this.isLocked = locked;
-    }
-
-    public boolean isLocked(){
-        return isLocked;
-    }
-
-    public void dispose()
-    {
-        doorTexture.dispose();
-    }
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * This method sets a doors lock status
+   * 
+   * @param locked a boolean value setting the door to locked or unlocked
+   */
+  public void setLocked(boolean locked) {
+    this.isLocked = locked;
+  }
+  
+  /**
+   * NEW FOR ASSESSMENT 2
+   * The method returns if a door is locked or not.
+   * 
+   * @return true if the door is locked, false otherwise
+   */
+  public boolean isLocked() {
+    return isLocked;
+  }
+  
+  /**
+   * This method disposes of the door texture.
+   */
+  public void dispose() {
+    doorTexture.dispose();
+  }
 }
