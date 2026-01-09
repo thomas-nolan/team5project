cfb8574b60a2b2706ce4949e75603ca8e1892a95
thomas-nolan <t.nolan2592@gmail.com>
2026-01-09 23:54:07 +0000

    Adding java doc and changing style to google style

--- Game/core/src/main/java/io/github/team10/escapefromuni/EventFreeze.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/EventFreeze.java
@@ -5,91 +5,144 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 /**
+ * NEW FOR ASSESSMENT 2.
  * Class for positive event.
- * This event freezes the timer and the dean for a fixed period of time
+ * This event freezes the timer and the dean for a fixed period of time.
+ * It is activated by collecting the freeze pickup.
  */
 public class EventFreeze implements IEvent {
 
-    private final Texture freezeTexture;
-    private Sprite freezeSprite;
-    private Timer timer;
-    private boolean eventFinished;
-    private final EventType type;
-    private Player player;
-    private EscapeGame game;
-    private final EventSystem eventSystem;
+  // Texture and sprite used to render the freeze event
+  private final Texture freezeTexture;
+  private Sprite freezeSprite;
 
-    private boolean used = false;
+  // References to core game systems required for the event
+  private Timer timer;
+  private boolean eventFinished;
+  private final EventType type;
+  private Player player;
+  private EscapeGame game;
 
-    public EventFreeze(Player player, EscapeGame game, Timer timer, EventSystem eventSystem) {
-        this.player = player;
-        this.game = game;
-        this.type = EventType.POSITIVE;
-        freezeTexture = new Texture("timer.png");
-        this.timer = timer;
-        this.eventFinished = false;
-        this.eventSystem = eventSystem;
+  // Tracks the events state
+  private final EventSystem eventSystem;
+  private boolean used = false;
+
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * Constructor used to create a new freeze event.
+   * 
+   * @param player the player controlled by the user
+   * @param game the main LibGDX game instance
+   * @param timer the games timer
+   * @param eventSystem the central events manager
+   */
+  public EventFreeze(Player player, EscapeGame game, Timer timer, EventSystem eventSystem) {
+    this.player = player;
+    this.game = game;
+    this.type = EventType.POSITIVE;
+    freezeTexture = new Texture("timer.png");
+    this.timer = timer;
+    this.eventFinished = false;
+    this.eventSystem = eventSystem;
+  }
+
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * Returns the type of event
+   */
+  @Override
+  public EventType getType() {
+    return type;
+  }
+
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * Intialises the freeze event.
+   */
+  public void startEvent() {
+    if (eventFinished) {
+      return;
     }
 
-    @Override
-    public EventType getType() {
-        return type;
+    // Creates a new sprite setting its texture, size and position
+    freezeSprite = new Sprite(freezeTexture);
+    freezeSprite.setSize(2f, 1f);
+    freezeSprite.setPosition(7.5f, 5.5f);
+    // AudioManager.getInstance().playEventSound(this.type);
+  }
+  
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * End the event, cleaning up any resources once the event has been used.
+   */
+  public void endEvent() {
+    if (!eventFinished && used) {
+      eventFinished = true;
+      freezeTexture.dispose();
     }
+  }
 
-    public void startEvent() {
-        if (eventFinished) return;
-
-        freezeSprite = new Sprite(freezeTexture);
-        freezeSprite.setSize(2f, 1f);
-        freezeSprite.setPosition(7.5f, 5.5f);
-        //AudioManager.getInstance().playEventSound(this.type);
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * Updates the events logic each frame.
+   */
+  public void update(float delta) {
+    if (!used) {
+      float playerDist = getPlayerFreezeDist();
+      if (playerDist < 1f) {
+        pickupFreeze();
+      }
     }
+  }
 
-    public void endEvent() {
-        if (!eventFinished && used) {
-            eventFinished = true;
-            freezeTexture.dispose();
-        }
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * Calculates the distance between the player and the freeze event pickup.
+   * 
+   * @return The distance between the player and the centre of the freeze pickup
+   */
+  private float getPlayerFreezeDist() {
+    Vector2 playerPos = player.getCenter();
+    float freezeX = freezeSprite.getX() + freezeSprite.getWidth() / 2f;
+    float freezeY = freezeSprite.getY() + freezeSprite.getHeight() / 2f;
+    Vector2 freezeCenter = new Vector2(freezeX, freezeY);
+    return freezeCenter.dst(playerPos);
+  }
+
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * Handles the collection of the freeze pick up.
+   * Registers the event type.
+   */
+  private void pickupFreeze() {
+    used = true;
+    // Implement here
+    eventSystem.registerEvent(type);
+    // Stops the timer upon pick up
+    timer.setFrozen();
+  }
+
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * Renders the freeze pickup while it has not been used.
+   */
+  public void draw() {
+    if (!used) {
+      freezeSprite.draw(game.batch);
     }
+  }
 
-    public void update(float delta) {
-        if (!used) {
-            float playerDist = getPlayerFreezeDist();
-            if (playerDist < 1f) {
-                pickupFreeze();
-            }
-        }
-    }
+  /** 
+   * NEW FOR ASSESSMENT 2.
+   * Used to draw the UI for the event.
+   * Not needed as there is no UI.
+   */
+  @Override
+  public void drawUI() {}
 
-    private float getPlayerFreezeDist() {
-        Vector2 playerPos = player.getCenter();
-        float freezeX = freezeSprite.getX() + freezeSprite.getWidth() / 2f;
-        float freezeY = freezeSprite.getY() + freezeSprite.getHeight() / 2f;
-        Vector2 freezeCenter = new Vector2(freezeX, freezeY);
-        return freezeCenter.dst(playerPos);
-    }
-
-    private void pickupFreeze() {
-        used = true;
-        // Implement here
-        eventSystem.registerEvent(type);
-        timer.setFrozen();
-    }
-
-    public void draw() {
-        if (!used) {
-            freezeSprite.draw(game.batch);
-        }
-    }
-
-    @Override
-    public void drawUI() {
-
-    }
-
-    @Override
-    public boolean IsFinished() {
-        return eventFinished;
-    }
+  @Override
+  public boolean IsFinished() {
+    return eventFinished;
+  }
 }
 
--- Game/core/src/main/java/io/github/team10/escapefromuni/EventGreggs.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/EventGreggs.java
@@ -6,111 +6,135 @@ import com.badlogic.gdx.math.Vector2;
 
 /**
  * Positive event where the player encounters a Greggs sausage roll.
- *
  * When the player runs into the sausage roll, they gain a speed increase.
  */
 public class EventGreggs implements IEvent {
 
-    private final Player player;
-    private final EscapeGame game;
-    private final EventType type;
-    private final EventSystem eventSystem;
+  private final Player player;
+  private final EscapeGame game;
+  private final EventType type;
+  private final EventSystem eventSystem;
 
-    private boolean eventFinished = false;
+  private boolean eventFinished = false;
 
-    private final Texture greggsTexture;
-    private Sprite greggsSprite;
+  private final Texture greggsTexture;
+  private Sprite greggsSprite;
 
-    private boolean used = false;
+  private boolean used = false;
 
-    private DifficultySetup diffSetup = new DifficultySetup();
+  // NEW FOR ASSESSMENT 2 - used to modify the speed boost
+  private DifficultySetup diffSetup = new DifficultySetup();
 
-    /**
-     * Creates a new EventGreggs.
-     */
-    public EventGreggs(Player player, EscapeGame game, EventSystem eventSystem)
-    {
-        this.player = player;
-        this.game = game;
-        this.type = EventType.POSITIVE;
-        greggsTexture = new Texture("GreggsSausageRoll.png");
-        this.eventSystem = eventSystem;
+  /**
+   * EXTENDS ON FROM ASSESSMENT 1.
+   * Creates a new EventGreggs and sets up the new systems created in assessment 2.
+   * Also sets up the other classes needed in the event.
+   */
+  public EventGreggs(Player player, EscapeGame game, EventSystem eventSystem) {
+    this.player = player;
+    this.game = game;
+    this.type = EventType.POSITIVE;
+    greggsTexture = new Texture("GreggsSausageRoll.png");
+    this.eventSystem = eventSystem;
+  }
+
+  /**
+   * Returns the type for the event.
+   */
+  @Override
+  public EventType getType() {
+    return type;
+  }
+
+  /**
+   * Returns boolean value to say if the event has finished or not.
+   */
+  @Override
+  public boolean IsFinished() {
+    return eventFinished;
+  }
+
+  /**
+   * Starts the event, and sets up the sprite.
+   */
+  @Override
+  public void startEvent() {
+    if (eventFinished) {
+      return;
+    } 
+
+    // Creates the greggs sprite, setting its size and position
+    // Also gets the instance of its sound
+    greggsSprite = new Sprite(greggsTexture);
+    greggsSprite.setSize(3f, 2f);
+    greggsSprite.setPosition(6.5f, 3.5f);
+    AudioManager.getInstance().playEventSound(this.type);
+  }
+
+  /**
+   * Cleans up the event resources once the pickup has been collected.
+   */
+  @Override
+  public void endEvent() {
+    if (!eventFinished && used) {
+      eventFinished = true;
+      greggsTexture.dispose();
     }
+  }
 
-     @Override
-    public EventType getType() {
-        return type;
+  /**
+   * Updates the event logic.
+   */
+  @Override
+  public void update(float delta) {
+    if (!used) {
+      // Gets the distance to the Greggs sprite and picks it up if the distance is less than 1
+      float playerDist = getPlayerGreggsDist();
+      if (playerDist < 1f) {
+        pickupGreggs();
+      }
     }
+  }
 
-    @Override
-    public boolean IsFinished() {
-        return eventFinished;
+  /**
+   * Calculates the distance between the center of the player and the Greggs sprite.
+   *
+   * @return the distance between the player and the Greggs sprite.
+   */
+  private float getPlayerGreggsDist() {
+    Vector2 playerPos = player.getCenter();
+
+    // Gets the coordinates of the greggs sprite
+    float greggsX = greggsSprite.getX() + greggsSprite.getWidth() / 2f;
+    float greggsY = greggsSprite.getY() + greggsSprite.getHeight() / 2f;
+    Vector2 greggsCenter = new Vector2(greggsX, greggsY);
+    return greggsCenter.dst(playerPos);
+  }
+
+  /**
+   * Handles applying the effects (speed increase) of collecting the Greggs sausage roll.
+   */
+  private void pickupGreggs() {
+    used = true;
+    eventSystem.registerEvent(EventType.POSITIVE);
+    // NEW FOR ASSESSMENT 2 - Modifies the players speed depending on the difficulty
+    player.increaseSpeed(2f * diffSetup.readDifficulty().speedBoostModifier);
+  }
+
+  /**
+   * Renders the Greggs pickup whilst its not picked it.
+   */
+  @Override
+  public void draw() {
+    if (!used) {
+      greggsSprite.draw(game.batch);
     }
+  }
 
-    @Override
-    public void startEvent() {
-        if (eventFinished) return;
-
-        greggsSprite = new Sprite(greggsTexture);
-        greggsSprite.setSize(3f, 2f);
-        greggsSprite.setPosition(6.5f, 3.5f);
-        AudioManager.getInstance().playEventSound(this.type);
-    }
-
-    @Override
-    public void endEvent() {
-        if (!eventFinished && used)
-        {
-            eventFinished = true;
-            greggsTexture.dispose();
-        }
-    }
-
-    @Override
-    public void update(float delta) {
-        if (!used)
-        {
-            float playerDist = getPlayerGreggsDist();
-            if (playerDist < 1f)
-            {
-                pickupGreggs();
-            }
-        }
-    }
-
-    /**
-     * Calculates the distance between the center of the player and the Greggs sprite.
-     *
-     * @return the distance between the player and the Greggs sprite.
-     */
-    private float getPlayerGreggsDist()
-    {
-        Vector2 playerPos = player.getCenter();
-        float greggsX = greggsSprite.getX() + greggsSprite.getWidth() / 2f;
-        float greggsY = greggsSprite.getY() + greggsSprite.getHeight() / 2f;
-        Vector2 greggsCenter = new Vector2(greggsX, greggsY);
-        return greggsCenter.dst(playerPos);
-    }
-
-    /**
-     * Handles applying the effects (speed increase) of collecting the Greggs sausage roll.
-     */
-    private void pickupGreggs()
-    {
-        used = true;
-        eventSystem.registerEvent(EventType.POSITIVE);
-        player.increaseSpeed(2f * diffSetup.readDifficulty().speedBoostModifier);
-    }
-
-    @Override
-    public void draw() {
-        if (!used)
-        {
-            greggsSprite.draw(game.batch);
-        }
-    }
-
-    @Override
-    public void drawUI() {}
+  /**
+   * Draws the UI elements, there are none currently.
+   */
+  @Override
+  public void drawUI() {}
 
 }
