d10484d6d90e39f1a82a4bd9ba5eab8ef5fa16c3
thomas-nolan <t.nolan2592@gmail.com>
2026-01-09 18:04:36 +0000

    Adding java doc and changing style to google style

--- Game/core/src/main/java/io/github/team10/escapefromuni/AchievementManager.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/AchievementManager.java
@@ -2,83 +2,115 @@ package io.github.team10.escapefromuni;
 
 import java.util.HashMap;
 
-
+/**
+ * NEW FOR ASSESSMENT 2
+ * This is a class which manages the achievements in the game.
+ */
 public class AchievementManager {
 
-    HashMap<String, Boolean> achievements = new HashMap<String, Boolean>();
+  HashMap<String, Boolean> achievements = new HashMap<String, Boolean>();
 
-    public AchievementManager(){
+  /**
+   * NEW FOR ASSESSMENT 2
+   * This constructor sets the achievements which you can get and sets them to false.
+   */
+  public AchievementManager() {
 
-        //ones that are commented out arent done yet bc 
-        //  they arent possible yet
-        //'No incorrect answers' is true by default, you lose the achievement
-        //  if you fail a question
+    // ones that are commented out arent done yet bc 
+    //  they arent possible yet
+    // 'No incorrect answers' is true by default, you lose the achievement
+    //  if you fail a question
 
-        achievements.put("Complete game", false);
-        achievements.put("Speedrun", false);
-        achievements.put("All positive events", false);
-        achievements.put("All negative events", false);
-        achievements.put("All hidden events", false);
-        achievements.put("Find Long Boi", false);
-        achievements.put("No incorrect answers", true);
+    achievements.put("Complete game", false);
+    achievements.put("Speedrun", false);
+    achievements.put("All positive events", false);
+    achievements.put("All negative events", false);
+    achievements.put("All hidden events", false);
+    achievements.put("Find Long Boi", false);
+    achievements.put("No incorrect answers", true);
 
-    }
-
-    public void addAchievement(String achievementName){
+  }
+  
+  /**
+   * NEW FOR ASSESSMENT 2
+   * This method adds achievements to the game.
+   * 
+   * @param achievementName The name of the achievement you wish to add
+   */
+  public void addAchievement(String achievementName) {
         
-        achievements.put(achievementName,true);
+    achievements.put(achievementName, true);
 
-    }
+  }
 
-    public void removeAchievement(String achievementName){
+  /**
+   * NEW FOR ASSESSMENT 2
+   * This method is used to mark achievements as not completed.
+   * 
+   * @param achievementName The name of the achievement you wish to remove
+   */
+  public void removeAchievement(String achievementName) {
         
-        achievements.put(achievementName,false);
+    achievements.put(achievementName, false);
 
+  }
+
+  /**
+   * NEW FOR ASSESSMENT 2
+   * Returns the number of completed achievements.
+   * 
+   * @return The number of achievements
+   */
+  public int getTotalAchievements() {
+
+    int total = 0;
+
+    for (Boolean i : achievements.values()) {
+      if (i) {
+        total += 1;
+      }
     }
 
-    public int getTotalAchievements(){
+    return total;
+  }
 
-        int total = 0;
+  /**
+   * NEW FOR ASSESSMENT 2
+   * Gets all the achievement the player has completed.
+   * 
+   * @return An array containing the completed achievements
+   */
+  public String[] getAchievements() {
 
-        for (Boolean i : achievements.values()){
-            if (i){
-                total += 1;
-            }
-        }
+    String[] currentAchievements = new String[getTotalAchievements()];
 
-        return total;
+    int index = 0;
+    for (String key : achievements.keySet()) {
+      if (achievements.get(key)) {    
+        currentAchievements[index] = key;
+        index += 1;
+
+      }
     }
 
-    public String[] getAchievements(){
+    return currentAchievements;
 
-        String[] currentAchievements = new String[getTotalAchievements()];
+  }
 
-        int index = 0;
-        for (String key : achievements.keySet()){
+  /**
+   * NEW FOR ASSESSMENT 2
+   * This resets all the achievements marking them as incomplete.
+   */
+  public void reset() {
 
-            if (achievements.get(key)){
-                
-                currentAchievements[index] = key;
-                index += 1;
+    achievements.put("Complete game", false);
+    achievements.put("Speedrun", false);
+    achievements.put("All positive events", false);
+    achievements.put("All negative events", false);
+    achievements.put("All hidden events", false);
+    achievements.put("Find Long Boi", false);
+    achievements.put("No incorrect answers", true);
 
-            }
-        }
-
-        return currentAchievements;
-
-    }
-
-    public void reset(){
-
-        achievements.put("Complete game", false);
-        achievements.put("Speedrun", false);
-        achievements.put("All positive events", false);
-        achievements.put("All negative events", false);
-        achievements.put("All hidden events", false);
-        achievements.put("Find Long Boi", false);
-        achievements.put("No incorrect answers", true);
-
-    }
-
+  }
 
 }
--- Game/core/src/main/java/io/github/team10/escapefromuni/AudioManager.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/AudioManager.java
@@ -6,167 +6,214 @@ import com.badlogic.gdx.audio.Sound;
 
 /**
  * AudioManager for handling all game audio.
- * Manages background music for menus and gameplay, and sound effects for different events and button clicks.
+ * Manages background music for menus and gameplay
+ * Manages sound effects for different events and button clicks.
  */
 public class AudioManager {
-    private static AudioManager instance;
+  private static AudioManager instance;
     
-    // Music tracks (looping background music)
-    private Music menuMusic;
-    private Music gameMusic;
+  // Music tracks (looping background music)
+  private Music menuMusic;
+  private Music gameMusic;
     
-    // Sound effects (played once when the even is triggered or button click)
-    private Sound clickSound;
-    private Sound negativeEventSound;
-    private Sound positiveEventSound;
-    private Sound hiddenEventSound;
+  // Sound effects (played once when the even is triggered or button click)
+  private Sound clickSound;
+  private Sound negativeEventSound;
+  private Sound positiveEventSound;
+  private Sound hiddenEventSound;
     
-    
-    private Music currentMusic;
-    private float currentVolume = 0.5f;
-    
-    
-    private AudioManager() {
-        // loads music and sound files from the files
-        loadAudio();
-    }
-    
-    //gets the audiomanager instance
-    public static AudioManager getInstance() {
-        if (instance == null) {
-            instance = new AudioManager();
-        }
-        return instance;
-    }
-    
-    // Loads all audio files
-    private void loadAudio() {
-        try {
-            // Load music tracks 
-            menuMusic = Gdx.audio.newMusic(Gdx.files.internal("MenuMusic.mp3"));
-            gameMusic = Gdx.audio.newMusic(Gdx.files.internal("GameMusic.mp3"));
-            
-            // for the gameplay and menu music to loop 
-            menuMusic.setLooping(true);
-            gameMusic.setLooping(true);
-            
-            // Set initial volume
-            menuMusic.setVolume(currentVolume);
-            gameMusic.setVolume(currentVolume);
-            
-            // Load sound effects
-            clickSound = Gdx.audio.newSound(Gdx.files.internal("Click.mp3"));
-            negativeEventSound = Gdx.audio.newSound(Gdx.files.internal("NegMus.mp3"));
-            positiveEventSound = Gdx.audio.newSound(Gdx.files.internal("PosMus.mp3"));
-            hiddenEventSound = Gdx.audio.newSound(Gdx.files.internal("HidMus.mp3"));
-            
-        } catch (Exception e) {
-            Gdx.app.error("AudioManager", "Error loading audio files: " + e.getMessage());
-        }
-    }
-    
-    // Plays menu music
-    public void playMenuMusic() {
-        if (currentMusic != menuMusic) {
-            stopCurrentMusic();
-            currentMusic = menuMusic;
-            menuMusic.play();
-        }
-    }
-    
-    // Plays game music
-    public void playGameMusic() {
-        if (currentMusic != gameMusic) {
-            stopCurrentMusic();
-            currentMusic = gameMusic;
-            gameMusic.play();
-        }
-    }
-    
-    /**
-     * Plays event sound effect based on event type.
-     * @param eventType The type of event (NEGATIVE, POSITIVE, or HIDDEN)
-     */
-    public void playEventSound(EventType eventType) {
-        Sound eventSound = null;
-        
-        switch (eventType) {
-            case NEGATIVE:
-                eventSound = negativeEventSound;
-                break;
-            case POSITIVE:
-                eventSound = positiveEventSound;
-                break;
-            case HIDDEN:
-                eventSound = hiddenEventSound;
-                break;
-            default:
-                break;
-        }
-        
-        if (eventSound != null) {
-            eventSound.play(currentVolume); 
-        }
-    }
-    
-    //click sound effect
-    public void playClickSound() {
-        if (clickSound != null) {
-            clickSound.play(currentVolume);
-        }
-    }
-    
-    // Stops the currently playing music
-    private void stopCurrentMusic() {
-        if (currentMusic != null && currentMusic.isPlaying()) {
-            currentMusic.stop();
-        }
-    }
-    
-    //pauses the currently playing music
-    public void pauseMusic() {
-        if (currentMusic != null && currentMusic.isPlaying()) {
-            currentMusic.pause();
-        }
-    }
-    
-    //resumes the currently playing music
-    public void resumeMusic() {
-        if (currentMusic != null && !currentMusic.isPlaying()) {
-            currentMusic.play();
-        }
-    }
-    
-    // Sets the master volume for all audio from 0.1f to 1.0f
-    public void setVolume(float volume) {
-        this.currentVolume = Math.max(0f, Math.min(1f, volume));
-        
-        if (menuMusic != null) menuMusic.setVolume(currentVolume);
-        if (gameMusic != null) gameMusic.setVolume(currentVolume);
-    }
+  private Music currentMusic;
+  private float currentVolume = 0.5f;
 
-    public float getVolume() {
-        return currentVolume;
-    }
+  private AudioManager() {
+    // loads music and sound files from the files
+    loadAudio();
+  }
     
-    //Sets the volume for every audio
-    
-    public void setMusicVolume(float volume) {
-        if (menuMusic != null) menuMusic.setVolume(volume);
-        if (gameMusic != null) gameMusic.setVolume(volume);
+  /**
+   * Gets the audio manager instance.
+   * 
+   * @return the instance of the audio manager
+   */
+  public static AudioManager getInstance() {
+    if (instance == null) {
+      instance = new AudioManager();
     }
-
-    public void setSoundVolume(float volume) {
-    }
+    return instance;
+  }
     
-    // Disposes all audio 
-    public void dispose() {
+  /**
+   * Loads all audio files into the game.
+   */
+  private void loadAudio() {
+    try {
+      // Load music tracks 
+      menuMusic = Gdx.audio.newMusic(Gdx.files.internal("MenuMusic.mp3"));
+      gameMusic = Gdx.audio.newMusic(Gdx.files.internal("GameMusic.mp3"));
+            
+      // for the gameplay and menu music to loop 
+      menuMusic.setLooping(true);
+      gameMusic.setLooping(true);
+            
+      // Set initial volume
+      menuMusic.setVolume(currentVolume);
+      gameMusic.setVolume(currentVolume);
+            
+      // Load sound effects
+      clickSound = Gdx.audio.newSound(Gdx.files.internal("Click.mp3"));
+      negativeEventSound = Gdx.audio.newSound(Gdx.files.internal("NegMus.mp3"));
+      positiveEventSound = Gdx.audio.newSound(Gdx.files.internal("PosMus.mp3"));
+      hiddenEventSound = Gdx.audio.newSound(Gdx.files.internal("HidMus.mp3"));
+            
+    } catch (Exception e) {
+      Gdx.app.error("AudioManager", "Error loading audio files: " + e.getMessage());
+    }
+  }
+    
+  /**
+   * Plays the menu music.
+   */
+  public void playMenuMusic() {
+    if (currentMusic != menuMusic) {
+      stopCurrentMusic();
+      currentMusic = menuMusic;
+      menuMusic.play();
+    }
+  }
+    
+  /**
+   * Plays the game music.
+   */
+  public void playGameMusic() {
+    if (currentMusic != gameMusic) {
+      stopCurrentMusic();
+      currentMusic = gameMusic;
+      gameMusic.play();
+    }
+  }
+    
+  /**
+   * Plays event sound effect based on event type.
+   * 
+   * @param eventType The type of event (NEGATIVE, POSITIVE, or HIDDEN)
+   */
+  public void playEventSound(EventType eventType) {
+    Sound eventSound = null;
         
-        if (menuMusic != null) menuMusic.dispose();
-        if (gameMusic != null) gameMusic.dispose();
-        if (clickSound != null) clickSound.dispose();
-        if (negativeEventSound != null) negativeEventSound.dispose();
-        if (positiveEventSound != null) positiveEventSound.dispose();
-        if (hiddenEventSound != null) hiddenEventSound.dispose();
+    switch (eventType) {
+      case NEGATIVE:
+        eventSound = negativeEventSound;
+        break;
+      case POSITIVE:
+        eventSound = positiveEventSound;
+        break;
+      case HIDDEN:
+        eventSound = hiddenEventSound;
+        break;
+      default:
+        break;
     }
+        
+    if (eventSound != null) {
+      eventSound.play(currentVolume); 
+    }
+  }
+    
+  /**
+   * Plays the click sound effect.
+   */
+  public void playClickSound() {
+    if (clickSound != null) {
+      clickSound.play(currentVolume);
+    }
+  }
+    
+  /**
+   * Stops the currently playing music.
+   */
+  private void stopCurrentMusic() {
+    if (currentMusic != null && currentMusic.isPlaying()) {
+      currentMusic.stop();
+    }
+  }
+    
+  /**
+   * Pauses the currently playing music.
+   */
+  public void pauseMusic() {
+    if (currentMusic != null && currentMusic.isPlaying()) {
+      currentMusic.pause();
+    }
+  }
+    
+  /**
+   * Resumes the currently playing music.
+   */
+  public void resumeMusic() {
+    if (currentMusic != null && !currentMusic.isPlaying()) {
+      currentMusic.play();
+    }
+  }
+    
+  /**
+   * Sets the master volume for all audio from 0.1f to 1.0f.
+   * 
+   * @param volume the value you wish to set it the volume to
+   */
+  public void setVolume(float volume) {
+    this.currentVolume = Math.max(0f, Math.min(1f, volume));
+        
+    if (menuMusic != null) {
+      menuMusic.setVolume(currentVolume);
+    } 
+    if (gameMusic != null) {
+      gameMusic.setVolume(currentVolume);
+    } 
+  }
+
+  public float getVolume() {
+    return currentVolume;
+  }
+    
+  /**
+   * Sets the volume for every audio.
+   * 
+   * @param volume the value you wish to set it the volume to
+   */
+    
+  public void setMusicVolume(float volume) {
+    if (menuMusic != null) {
+      menuMusic.setVolume(volume);
+    } 
+    if (gameMusic != null) {
+      gameMusic.setVolume(volume);
+    }
+  }
+
+  // public void setSoundVolume(float volume) {}
+    
+  /**
+   * Disposes of all the audio. 
+   */
+  public void dispose() {
+        
+    if (menuMusic != null) {
+      menuMusic.dispose();
+    }
+    if (gameMusic != null) {
+      gameMusic.dispose();  
+    } 
+    if (clickSound != null) {
+      clickSound.dispose();
+    }
+    if (negativeEventSound != null) {
+      negativeEventSound.dispose();
+    }
+    if (positiveEventSound != null) {
+      positiveEventSound.dispose();
+    }
+    if (hiddenEventSound != null) {
+      hiddenEventSound.dispose();
+    }
+  }
 }
