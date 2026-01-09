a4bd4085854a6e4c9e638616b09674d30775e71f
thomas-nolan <t.nolan2592@gmail.com>
2026-01-09 19:35:10 +0000

    Adding java doc and changing style to google style

--- Game/core/src/main/java/io/github/team10/escapefromuni/Dean.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/Dean.java
@@ -4,68 +4,115 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
-public class Dean extends Player{
+/**
+ * NEW FOR ASSESSMENT 2.
+ * This class contains the logic for the dean
+ */
+public class Dean extends Player { 
 
-    private boolean reached = false;
-    private final float roomHeight;
-    private final float roomWidth;
-    private Texture deanTexture;
+  private boolean reached = false;
+  private final float roomHeight;
+  private final float roomWidth;
+  private Texture deanTexture;
 
-    private DifficultySetup difficultySetup = new DifficultySetup();
+  private DifficultySetup difficultySetup = new DifficultySetup();
 
-    public Dean(float speed, float width, float height, EscapeGame game, float roomWidth, float roomHeight){
-        super(speed, width, height, game);
-        this.roomWidth = roomWidth;
-        this.roomHeight = roomHeight;
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * This constuctor sets up the implementation of the dean
+   * 
+   * @param speed the speed at which the dean moves
+   * @param width the width of the dean
+   * @param height the height of the dean
+   * @param game the main LibGDX game instance
+   * @param roomWidth the width of the room is in
+   * @param roomHeight the height of the room the dean is in
+   */
+  public Dean(float speed, float width, float height, 
+       EscapeGame game, float roomWidth, float roomHeight) {
+    super(speed, width, height, game);
+    this.roomWidth = roomWidth;
+    this.roomHeight = roomHeight;
+    
+    // creates the Dean's sprite and sets its texture and size
+    deanTexture = new Texture("dean.png");
+    playerSprite.setTexture(deanTexture);
+    playerSprite.setSize(width, height);
 
-        deanTexture = new Texture("dean.png");
-        playerSprite.setTexture(deanTexture);
-        playerSprite.setSize(width, height);
+    // Places the Dean in the centre of the room
+    setCenter(roomWidth / 2f, roomHeight / 2f);
+  }
 
-        setCenter(roomWidth / 2f, roomHeight / 2f);
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * This controlls the logic for the dean
+   * Gets the dean to be constantly chasing the user
+   */
+  @Override
+  public void update(float delta) {
+    if (reached) {
+      return;
+    } 
+    chasePlayer(delta);
+    super.update(delta);
+  }
+
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * This handles the logic for the dean chasing the user
+   * The Dean moves towards the player each frame
+   * 
+   * @param delta the time elapsed since the last frame in seconds
+   */
+  private void chasePlayer(float delta) {
+
+    // Get current position of the Dean and the user
+    Player player = game.gameController.getPlayer();
+    Vector2 playerPos = player.getCenter();
+    Vector2 deanPos = getCenter();
+  
+    // Calculates the direction vector of the player and the Dean
+    float dx = playerPos.x - deanPos.x;
+    float dy = playerPos.y - deanPos.y;
+
+    float distance = deanPos.dst(playerPos);
+
+    // Enables movement if the Dean is not on the player
+    if (distance > 0) {
+      float speedModifier = difficultySetup.readDifficulty().enemySpeedModifier;
+      float moveX = (dx / distance) * speed * delta * speedModifier;
+      float moveY = (dy / distance) * speed * delta * speedModifier;
+      playerSprite.translateX(moveX);
+      playerSprite.translateY(moveY);
     }
+  }
 
-    @Override
-    public void update(float delta){
-        if(reached) return;
-
-        chasePlayer(delta);
-
-        super.update(delta);
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * This is a simple check to see if the Dean has caught the player
+   * 
+   * @param player the player controlled by the user
+   * @return a boolean value stating if the player is caught
+   */
+  public boolean checkReached(Player player) {
+    // Checks if the Dean and player is touching
+    // Returns false if the player has the invincibilty event triggered
+    if (!reached && player.checkCollision(this.playerSprite) && !player.isInvincible()) {
+      reached = true;
+      return true;
     }
+    return false;
+  }
 
-    private void chasePlayer(float delta){
-
-        Player player = game.gameController.getPlayer();
-        Vector2 playerPos = player.getCenter();
-        Vector2 deanPos = getCenter();
-
-        float dx = playerPos.x - deanPos.x;
-        float dy = playerPos.y - deanPos.y;
-
-        float distance = deanPos.dst(playerPos);
-
-        if (distance > 0){
-            float moveX = (dx / distance) * speed * delta * difficultySetup.readDifficulty().enemySpeedModifier;
-            float moveY = (dy / distance) * speed * delta * difficultySetup.readDifficulty().enemySpeedModifier;
-            playerSprite.translateX(moveX);
-            playerSprite.translateY(moveY);
-        }
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * This disposes of the Deans texture
+   */
+  @Override
+  public void dispose() {
+    super.dispose();
+    if (deanTexture != null) {
+      deanTexture.dispose();
     }
-
-    public boolean checkReached(Player player){
-        if (!reached && player.checkCollision(this.playerSprite) && !player.isInvincible()){
-            reached = true;
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void dispose(){
-        super.dispose();
-        if (deanTexture != null){
-            deanTexture.dispose();
-        }
-    }
+  }
 }
