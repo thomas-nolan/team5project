2d90218485796da032e71f65b81e4fca63a219b7
thomas-nolan <t.nolan2592@gmail.com>
2026-01-10 15:29:38 +0000

    Adding java doc and changing style to google style

--- Game/core/src/main/java/io/github/team10/escapefromuni/IEvent.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/IEvent.java
@@ -1,42 +1,51 @@
 package io.github.team10.escapefromuni;
 
 /**
- * Abstract base event class.
+ * NEW FOR ASSESSMENT 2.
+ * Interface representing an in game event.
  * Events are contained within a {@link Room}, where there can be up to 1 event per room.
  */
 public interface IEvent {
-    EventType getType();
+  /**
+   * EXTENDED FROM ASSESSMENT 1.
+   * Returns the type of the event.
+   * 
+   * @return the type of event
+   */
+  EventType getType();
 
-    /**
-     * Called when the event starts (e.g. the player enters the room).
-     */
-    void startEvent();
+  /**
+   * Called when the event starts (e.g. the player enters the room).
+   */
+  void startEvent();
 
-    /**
-     * Called when the event should end. 
-     * 
-     * Disposes of resources and restores normal gameplay.
-     */
-    void endEvent();
+  /**
+   * Called when the event should end.  
+   * 
+   * <P>Disposes of resources and restores normal gameplay.
+   * </P>
+   */
+  void endEvent();
 
-    /**
-     * Called every frame to update the event's logic.
-     * @param delta The time elapsed since the last frame in seconds.
-     */
-    void update(float delta);
+  /**
+   * Called every frame to update the event's logic.
+   * 
+   * @param delta The time elapsed since the last frame in seconds.
+   */
+  void update(float delta);
 
-    /**
-     * Draws world-space graphics related to the event.
-     */
-    void draw();
+  /**
+   * Draws world-space graphics related to the event.
+   */
+  void draw();
 
-    /**
-     * Draws UI-space graphics for the event.
-     */
-    void drawUI();
+  /**
+   * Draws UI-space graphics for the event.
+   */ 
+  void drawUI();
 
-    /**
-     * Returns whether the event has finished.
-     */
-    boolean IsFinished();
+  /**
+   * Returns whether the event has finished.
+   */
+  boolean IsFinished();
 }
--- Game/core/src/main/java/io/github/team10/escapefromuni/InvincibleEvent.java
+++ Game/core/src/main/java/io/github/team10/escapefromuni/InvincibleEvent.java
@@ -6,71 +6,91 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public class InvincibleEvent implements IEvent{
-    private final Player player;
-    private final EscapeGame game;
-    private boolean finished = false;
-    private boolean pickedUp = false;
-    private Texture texture;
-    private Sprite sprite;
-    private final EventSystem eventSystem;
+/**
+ * NEW FOR ASSESSMENT 2.
+ * This is a positive event that gives the player immunity
+ * to certain negative events such as the dean and the trap.
+ */
+public class InvincibleEvent implements IEvent {
+  private final Player player;
+  private final EscapeGame game;
+  private boolean finished = false;
+  private boolean pickedUp = false;
+  private Texture texture;
+  private Sprite sprite;
+  private final EventSystem eventSystem;
 
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * Creates a new Invincible event and sets up the systems and textures
+   * needed.
+   * 
+   * @param game the main LibGDX game instance
+   * @param eventSystem the system that handles the central logic for events
+   */
+  public InvincibleEvent(Player player, EscapeGame game, EventSystem eventSystem) {
 
-    public InvincibleEvent(Player player, EscapeGame game, EventSystem eventSystem){
-        this.player = player;
-        this.game = game;
-        this.eventSystem = eventSystem;
+    // Initialises the player, game and eventSystem used for the event
+    this.player = player;
+    this.game = game;
+    this.eventSystem = eventSystem;
 
-        texture = new Texture("shield.png");
-        sprite = new Sprite(texture);
-        sprite.setSize(1.5f, 1.5f);
-        float width = game.viewport.getWorldWidth();
-        float height = game.viewport.getWorldHeight();
-        sprite.setPosition(width / 2f - sprite.getWidth(), height / 2f - sprite.getHeight());
+    // Sets the texture as well as the sprite
+    texture = new Texture("shield.png");
+    sprite = new Sprite(texture);
+    sprite.setSize(1.5f, 1.5f);
+
+    // Calculates and sets the position of the pick up
+    float width = game.viewport.getWorldWidth();
+    float height = game.viewport.getWorldHeight();
+    sprite.setPosition(width / 2f - sprite.getWidth(), height / 2f - sprite.getHeight());
+  }
+
+  @Override
+  public EventType getType() {
+    return EventType.POSITIVE;
+  }
+
+  @Override
+  public void startEvent() {}
+
+  @Override
+  public void endEvent() {
+    texture.dispose();
+  }
+
+  /**
+   * NEW FOR ASSESSMENT 2.
+   * This updates the logic each frame for the event
+   */
+  @Override
+  public void update(float delta) {
+    if (finished) {
+      return;
     }
-
-    @Override
-    public EventType getType() {
-        return EventType.POSITIVE;
+    if (!pickedUp) {
+      // Checks to see it the player picks up the event and adds it to the event counter
+      if (player.checkCollision(sprite)) {
+        pickedUp = true;
+        eventSystem.registerEvent(EventType.POSITIVE);
+        player.setInvincible(10f);
+      } 
     }
+  }
 
-    @Override
-    public void startEvent() {
+  @Override
+  public void draw() {
+    if (!pickedUp) {
+      sprite.draw(game.batch);
     }
+  }
 
-    @Override
-    public void endEvent() {
-       texture.dispose();
-    }
+  @Override
+  public void drawUI() {}
 
-    @Override
-    public void update(float delta) {
-        if(finished) return;
-
-
-        if(!pickedUp){
-            if(player.checkCollision(sprite)){
-                pickedUp = true;
-                eventSystem.registerEvent(EventType.POSITIVE);
-                player.setInvincible(10f);
-            } 
-        }
-    }
-
-    @Override
-    public void draw() {
-       if(!pickedUp){
-        sprite.draw(game.batch);
-       }
-    }
-
-    @Override
-    public void drawUI() {
-    }
-
-    @Override
-    public boolean IsFinished() {
-        return finished;
-    }
+  @Override
+  public boolean IsFinished() {
+    return finished;
+  }
     
 }
